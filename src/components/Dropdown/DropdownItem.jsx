d73669394b504223c893c3f3145895e1fcbb8dbc
IlyaAlpatov <alpatov30@gmail.com>
2023-03-24 19:29:40 +0300

    добавлены пропсы с типизацией в коспонент DropdownItem

--- src/components/Dropdown/DropdownItem.jsx
+++ src/components/Dropdown/DropdownItem.jsx
@@ -1,9 +1,19 @@
-const DropdownItem = () => {
+import PropTypes from "prop-types";
+
+const DropdownItem = ({ item }) => {
+  const { text, link } = item;
   return (
     <li>
-      <a href="#">Change Password</a>
+      <a href={link}>{text}</a>
     </li>
   );
+};
+
+DropdownItem.propTypes = {
+  item: PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default DropdownItem;
