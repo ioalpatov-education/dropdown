088dd874e1cd5eafaa58461e34f998097998878a
IlyaAlpatov <alpatov30@gmail.com>
2023-03-24 19:25:33 +0300

    добавлена проверка типов для dropdownlist

--- src/components/Dropdown/DropdownList.jsx
+++ src/components/Dropdown/DropdownList.jsx
@@ -1,5 +1,16 @@
-const DropdownList = () => {
+import PropTypes from "prop-types";
+
+const DropdownList = ({ items }) => {
   return <ul data-id="dropdown" className="dropdown"></ul>;
+};
+
+DropdownList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export default DropdownList;
